612c554ef2b64493653a725121ba266bc20c0da6
orKonijn <89410550+orKonijn@users.noreply.github.com>
2026-05-02 15:15:14 +0300

    Update ebay_site.py

--- src/ebay/pages/ebay_site.py
+++ src/ebay/pages/ebay_site.py
@@ -5,13 +5,13 @@ from playwright.sync_api import Page, expect
 
 from ebay.pages.advanced_search import AdvancedSearch
 from ebay.pages.base_page import BasePage
+from ebay.pages.cart_page import CartPage
 from ebay.pages.header_page import HeaderPage
 from ebay.pages.item_page import ItemPage
-from ebay.pages.cart_page import CartPage
 
 
 class EbaySite(BasePage):
-    """eBay home page interactions."""
+    """Top-level page object wiring for eBay flows."""
 
     def __init__(self, page: Page) -> None:
         super().__init__(page)
@@ -21,8 +21,8 @@ class EbaySite(BasePage):
         self.cart_page = CartPage(page)
 
     @allure.step("Open eBay home page")
-    def open(self, url: str = "https://www.ebay.com/") -> "HomePage":
+    def open(self, url: str = "https://www.ebay.com/") -> "EbaySite":
         self.page.goto(url, wait_until="domcontentloaded")
-        expect(self.page).to_have_url(url)
-        expect(self.page).to_have_title(re.compile("eBay", re.IGNORECASE))
+        expect(self.page).to_have_url(re.compile(r"ebay\.com", re.IGNORECASE))
+        expect(self.page).to_have_title(re.compile(r"eBay", re.IGNORECASE))
         return self
